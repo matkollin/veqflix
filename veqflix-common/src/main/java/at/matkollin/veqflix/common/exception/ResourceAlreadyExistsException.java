cf6415c3cbad64cc210367c2d93e37f736454308
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-02 18:45:43 +0100

    Add response status

--- veqflix-common/src/main/java/at/matkollin/veqflix/common/exception/ResourceAlreadyExistsException.java
+++ veqflix-common/src/main/java/at/matkollin/veqflix/common/exception/ResourceAlreadyExistsException.java
@@ -1,10 +1,14 @@
 package at.matkollin.veqflix.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * ResourceAlreadyExistsException is the superclass of those exceptions that can be thrown whenever a resource
  * is expected not to exist, but does.
  * The resource itself should be on a normal case an identifier or unique value such as a username or email.
  */
+@ResponseStatus(HttpStatus.CONFLICT)
 public class ResourceAlreadyExistsException extends RuntimeException {
 
   public ResourceAlreadyExistsException(String message) {
--- veqflix-common/src/main/java/at/matkollin/veqflix/common/exception/ResourceNotFoundException.java
+++ veqflix-common/src/main/java/at/matkollin/veqflix/common/exception/ResourceNotFoundException.java
@@ -1,10 +1,14 @@
 package at.matkollin.veqflix.common.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * ResourceNotFoundException is the superclass of those exceptions that can be thrown whenever a resource
  * is expected to exist, but doesn't.
  * The resource itself should be on a normal case an identifier or unique value such as a username or email.
  */
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
 
   public ResourceNotFoundException(String message) {
