7846db05edac0dfaacc251d4812ae6aaa1642a79
Matthias Kopeinigg <matkollin@gmail.com>
2022-11-02 18:33:37 +0100

    Add error response dto

---
+++ veqflix-common/src/main/java/at/matkollin/veqflix/common/http/ErrorResponse.java
@@ -0,0 +1,27 @@
+package at.matkollin.veqflix.common.http;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import java.time.ZonedDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+  private HttpStatus httpStatus;
+  private String message;
+
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd@HH:mm:ss.SSSZ")
+  @JsonSerialize(using = ZonedDateTimeSerializer.class)
+  private ZonedDateTime timestamp;
+
+}
